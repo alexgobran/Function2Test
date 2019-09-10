969f611630a1c9d3e5b203c59bd47721198abe97
alexgobran <54282581+alexgobran@users.noreply.github.com>
2019-09-10 09:06:15 -0400

    initial commit

---
+++ Function2Test/ForecastingFunction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Function2Test {
+
+
+    //Using testing
+
+    public class ForecastingFunction {
+        public int Multiplier { get; set; } = 0;
+        public int Forecast(int x, int y) {
+            if (x< -10 || x>10|| y < -10 || y > 10)
+            {
+                throw new Exception("X or Y are outside the domain");
+
+            }
+        return ((x* x * x) + (y* y)) * Multiplier;
+
+      }
+
+
+    }
+}
--- Function2Test/Program.cs
+++ Function2Test/Program.cs
@@ -2,8 +2,43 @@
 
 namespace Function2Test {
     class Program {
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            var ff = new ForecastingFunction();
+
+            //Test 1;
+            try {
+                //Multiplier is 0
+                var result = ff.Forecast(-11, 11); // expect: Exception!
+                Console.WriteLine("Test Failed");
+            } catch (Exception ex) {
+                Console.WriteLine("Test Passed!");
+
+            }
+            //Test 2;
+            // Multiplier is 2, x is 25, y is 5, result
+            try
+            {
+                ff.Multiplier = 2;
+                    var result = ff.Forecast(25, 5);
+                Console.WriteLine("Test Failed");
+            }catch (Exception ex) {
+                Console.WriteLine("Test Passed");
+            }
+
+            {
+                ff.Multiplier = 2;
+                var result = ff.Forecast(2, 2);
+                if (result ==24)
+                {
+                    Console.WriteLine("Test Passed!");
+
+                }else {
+                    Console.WriteLine("Test Failed");
+                        }
+            }
+
+            }
         }
     }
-}
+
